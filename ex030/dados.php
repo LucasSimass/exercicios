65464621c29d6d9501867b1d2503135cbd03d268
LucasSimass <simaslucas1m@gmail.com>
2024-06-28 17:12:23 -0300

    Novos exercícios

--- ex030/dados.php
+++ ex030/dados.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
+    <title>Dados</title>
 </head>
 <body>
-    <h1>Seus dados estão aqui</h1>
+    <h1>oi</h1>
 </body>
 </html>
